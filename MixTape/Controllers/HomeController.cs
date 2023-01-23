994792a035e471a2b11f0fc20b92e9843267bf80
zzaynAli <bcsf19a022@pucit.edu.pk>
2023-01-24 01:46:27 +0500

    Made Home Page and Login_SignUp

---
+++ MixTape/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MixTape.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MixTape.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult login_signup()
+        {
+            return View();
+        }
+
+    }
+}
